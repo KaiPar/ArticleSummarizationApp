121be845fcbdb82fc79c6f87d48df2649745de7d
Kairav Parekh <9643321zapdos@gmail.com>
2020-01-27 20:25:40 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.newssummarizationapp;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.*;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import org.json.*;
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mQueue = Volley.newRequestQueue(this);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    public void toastMsg(String article){
+        Toast toast = Toast.makeText(this, article, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void getSummary(View v){
+        final Resources res = getResources();
+        EditText txt = (EditText) findViewById(R.id.txtArticle);
+        final TextView summary = (TextView) findViewById(R.id.txtSummary);
+        summary.setMovementMethod(new ScrollingMovementMethod());
+        String article = txt.getText().toString();
+        String url ="http://192.168.43.140:5000/process_request?inp1=" + article;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            String msg1 = obj.getString(res.getString(R.string.message));
+                            Log.d("message is: ", msg1);
+                            message sum = new message(msg1);
+                            summary.setText(sum.giveSummary());
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(stringRequest);
+
+    }
+
+}
---
+++ message.java
@@ -0,0 +1,13 @@
+package com.example.newssummarizationapp;
+
+public class message {
+    String summary;
+
+    public message(String summary){
+        this.summary = summary;
+    }
+
+    public String giveSummary(){
+        return summary;
+    }
+}
